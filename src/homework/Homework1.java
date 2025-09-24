f2443686b05803f5accae8ae44ad260dd01bbab1
AnahitMarti <contact.anahitmartirosyan@gmail.com>
2025-09-24 22:30:08 +0200

    Homework1

---
+++ src/homework/Homework1.java
@@ -0,0 +1,49 @@
+package homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+//xndir 1
+
+        int x = 10;
+        int y = 20;
+        if (x < y) {
+            System.out.println("Y");
+        }
+        if (x > y) {
+            System.out.println("Y");
+
+        }
+        if (x == y) {
+            System.out.println("Y");
+        }
+
+//xndir 2
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println();
+
+        //xndir 3
+
+        int a = 5;
+        int b = 7;
+        int c = a + b;
+
+        System.out.println(c);
+
+
+        //xndir 4
+        int n = 4;
+        for (int v = 1; v <= 10; v++) {
+            System.out.println(n + " * " + v + "  =  " + (n * v));
+        }
+    }
+}
+
+
+
+
+
+
