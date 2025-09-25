9f70c24af70046cec5dd20d1583c1c66c91b1c47
AnahitMarti <contact.anahitmartirosyan@gmail.com>
2025-09-25 21:20:34 +0200

    Delete src/lesson3 directory

--- src/lesson3/CharExample.java
+++
@@ -1,9 +0,0 @@
-package lesson3;
-
-public class CharExample {
-    public static void main(String[] args) {
-        char a = '*';
-        for (int i = 0; i<5; i++)
-        System.out.print(a + " ");
-    }
-}
