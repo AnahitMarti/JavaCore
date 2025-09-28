76572e13ec7cd77dcd577db3250994dd2596f430
AnahitMarti <contact.anahitmartirosyan@gmail.com>
2025-09-28 16:34:56 +0200

    Sort homework

---
+++ src/homework/Sort.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                }
+            }
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
+
+
